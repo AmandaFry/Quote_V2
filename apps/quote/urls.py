3da5088b97b6394b8d8601cd44f872243c03f50c
Amanda Fry <afry@efry.org>
2016-06-02 16:13:43 -0700

    works to see new update, it reads in qupte and author

--- apps/quote/urls.py
+++ apps/quote/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
 	url(r'^clear/$', views.clear, name='clear'),
 	# url(r'^updateMe/$', views.updateMe, name='updateMe'),
 	url(r'^new/$', views.new, name='new'),
-	# url(r'^addNew/$', views.addNew, name='addNew'),
+	url(r'^addNew/$', views.addNew, name='addNew'),
 	url(r'^$', views.index, name='index'),
 ]
 
