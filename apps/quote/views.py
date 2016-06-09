a18bfd5ab770d66274081775676014b883518c8e
Amanda Fry <afry@efry.org>
2016-06-08 17:17:23 -0700

    Fixed the problem casused by SQL Lite not reusing the ID.

--- apps/quote/views.py
+++ apps/quote/views.py
@@ -2,34 +2,35 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse, Http404
 from .models import Category, Quote
 from django.utils import timezone
+from django.core.exceptions import ObjectDoesNotExist
 
 
 def index(request):
+	#this try - exept is for to see if the session count exists
 	try:#make the assumption session exists first 
  		if (request.session['count'] < request.session['counter1']):
 			request.session['count'] = request.session['count'] + 1
 			print 'I am ', request.session['count'], ' of ', request.session['counter1'], ' of quotes'
 		else:
 			request.session['count'] = 1
-<<<<<<< HEAD
+
 	except:#this will only happens one, if you don't have this the first time the page will not render
 		request.session['count'] = 1
+		
+	#this try - exept is for see if the record exists, if not then increase the count
+	# and redirect 
+	try:
+		secondq = Quote.objects.get(id=request.session['count'])
+		pass
+		context = {
+ 		"quote" : secondq,
+ 		}
+		return render(request, 'quote/index.html', context)
+	except:
+		print 'I got to except'
+		request.session['count'] + 1
+		return redirect ('/')
 
-	secondq = Quote.objects.get(id=request.session['count'])
-=======
-			# request.session['counter1'] = Quote.objects.count()
-	except:#this will only happens one, if you don't have this the first time the page will not render
-		request.session['count'] = 1
-
-	firstq = Quote.objects.get(id=request.session['count'])
-
->>>>>>> nextv
- 	context = {
- 		"quote" : firstq,
- 	}
-	return render(request, 'quote/index.html', context)
-
-<<<<<<< HEAD
 
 def clear(request):
 	#returns to first record
@@ -38,11 +39,8 @@ def clear(request):
 
 
 def new(request):
-	# bring up all category the records and render add new page
-=======
-def new(request):
 	#brings back all the categroies and display it drop down list
->>>>>>> nextv
+	request.session['is_new'] = True
 	allcategories = Category.objects.all()
 	context = {
 		'categories' : allcategories,
@@ -51,11 +49,7 @@ def new(request):
 
 
 def addNew(request):
-<<<<<<< HEAD
-	#add the new record - read in from - request.POST is request.form
-=======
 	#read in from - request.POST is request.form
->>>>>>> nextv
 	qouteforme = request.POST['quote']
 	authorforme = request.POST['author']
 	#I was not able to use the category id directly ... 
@@ -68,16 +62,19 @@ def addNew(request):
 	newquote.save()
 	#update the counter1 the number of records in the db
 	request.session['counter1'] = Quote.objects.count()
-<<<<<<< HEAD
 	print request.session['counter1']
 	#counterModified is needed in the successAE view
 	request.session['counterModified'] = request.session['counter1']
+	print request.session['counterModified']
 	return redirect ('/successAE')
 
 
 def edit(request):
-	#render the edit page
-	editq = Quote.objects.get(id=request.session['count'])
+	#render the edit page 
+	if request.session['is_new'] == True:
+		editq = Quote.objects.latest('id')
+	else:
+		editq = Quote.objects.get(id=request.session['count'])
 		#trying to preselect the category as a default that was assigned to the quote
 		# selected_cat = Category.objects.all().filter(category=editq.category_id.category)
 		# selected_cat = Category.objects.get(id=editq.category_id.category)
@@ -93,10 +90,12 @@ def edit(request):
 	 		# 'selected_category' : selected_cat,
  		'categories' : allcategories,
  	}
+ 	request.session['is_new'] = False
 	return render(request, 'quote/edit.html', context)
 
+
 def addEdit(request):
-	#Make sure I work with the correc record
+	#Make sure I work with the correct record
 	updatequote = Quote.objects.get(id=request.session['count'])
 	#Read in from the form quote and author
 	qouteforme = request.POST['quote']
@@ -117,10 +116,14 @@ def addEdit(request):
 def successAE(request):
 	#when add or edit record it will show the effected record in the home page
 	#Note this works fine for edit, but not for new not sure why.
-	sucq = Quote.objects.get(id=request.session['counterModified'])
+	if request.session['is_new'] == True:
+		sucq = Quote.objects.latest('id')
+	else:
+		sucq = Quote.objects.get(pk=request.session['counterModified'])
 	print ('%' * 25)
 	print "inside successAE"
 	print request.session['counterModified']
+	print sucq
 	print ('%' * 25)
  	context = {
  		"quote" : sucq,
@@ -137,36 +140,4 @@ def favorite(request):
 	return render(request, 'quote/Favorite.html', context)
 
 
-
-=======
-	return redirect ('/')
-
-
-def edit(request):
-	editq = Quote.objects.get(id=request.session['count'])
-	#trying to preselect the category as a default that was assigned to the quote
-	# selected_cat = Category.objects.all().filter(category=editq.category_id.category)
-	# selected_cat = Category.objects.get(id=editq.category_id.category)
-	# print selected_cat
-	# selected_cat = Category.objects.get(id=6)
-	# selected_cat = 6
-	# print ("*" *20)
-	# print selected_cat
-	# print ("*" *20)
-	allcategories = Category.objects.all()
-	context = {
- 		'quote' : editq,
- 		# 'selected_category' : selected_cat,
- 		'categories' : allcategories,
-
- 	}
-	return render(request, 'quote/edit.html', context)
-
-
-# def showOne(request):
-# 	print ("*" *20)
-# 	print 'I am inside the redirect'
-# 	print ("*" *20)
-# 	return render(request, 'quote/showOne.html')
->>>>>>> nextv
 
