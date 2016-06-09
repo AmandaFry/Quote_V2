eb456ce193b2a2d60dee76256cdf071081c29a15
Amanda Fry <afry@efry.org>
2016-06-08 17:16:07 -0700

    added Edit url

--- apps/quote/urls.py
+++ apps/quote/urls.py
@@ -6,10 +6,7 @@ urlpatterns = [
 	url(r'^favorite/$', views.favorite, name='favorite'),
 	url(r'^clear/$', views.clear, name='clear'),
 	url(r'^updateMe/$', views.edit, name='edit'),
-<<<<<<< HEAD
 	url(r'^addEdit/$', views.addEdit, name='addEdit'),
-=======
->>>>>>> nextv
 	url(r'^new/$', views.new, name='new'),
 	url(r'^addNew/$', views.addNew, name='addNew'),
 	url(r'^successAE/$', views.successAE, name='successAE'),
